9b156410ad8b22a680dc8f10e149983fee0c2ee0
AllusInteractive <AllusInteractive@users.noreply.github.com>
2022-07-09 20:19:50 +0100

    updated CSS for storybook

--- libs/component-library/src/lib/base-styles.tsx
+++ libs/component-library/src/lib/base-styles.tsx
@@ -5,8 +5,8 @@ export const BaseStyles = createGlobalStyle`
         box-sizing: border-box;
     }
     body {
-        padding: 0;
-        margin: 0;
+        padding: 0 !important;
+        margin: 0 !important;
         font-family: 'Barlow Condensed Regular', sans-serif;
     }
     a {
