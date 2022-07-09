ff83cb69d62392c530668011748759795d788c09
AllusInteractive <AllusInteractive@users.noreply.github.com>
2022-07-09 20:17:59 +0100

    added settings

---
+++ apps/allus-interactive/settings/colours.ts
@@ -0,0 +1,10 @@
+const colourValues = {
+  background: "#1f1f1f",
+  title: "#dd0207", //#c31014
+  primary: "#1F3CA6",
+  secondary: "#ffffff",
+  black: "#000000",
+  white: "#ffffff"
+};
+
+export default colourValues
