24600966f95dac60a6efbfa312c7aa721d2d0ec2
AllusInteractive <AllusInteractive@users.noreply.github.com>
2022-07-10 21:41:53 +0100

    moved header and footer to app.tsx

--- apps/allus-interactive/pages/_app.tsx
+++ apps/allus-interactive/pages/_app.tsx
@@ -1,16 +1,34 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { Header, Footer } from '@allus-interactive/component-library'
+import colourValues from '../settings/colours'
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const linkData = [
+    { url: '/rpgs', text: 'RPGs' },
+    { url: '/platformers', text: 'Platformers' },
+    { url: '/demos-and-prototypes', text: 'Demos/Prototype' },
+  ];
+
   return (
     <>
       <Head>
         <title>Welcome to Allus Interactive</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Header
+        bgColour={colourValues.background}
+        titleColour={colourValues.title}
+        hoverColour={colourValues.white}
+        links={linkData}
+        mobileImageUrl="/images/allus-logo.png"
+        desktopImageUrl="/images/allus-banner.png"
+      />
+      <Component {...pageProps} />
+      <Footer
+        bgColour={colourValues.background}
+        textColour={colourValues.white}
+      />
     </>
   );
 }
--- apps/allus-interactive/pages/index.tsx
+++ apps/allus-interactive/pages/index.tsx
@@ -1,43 +1,35 @@
 import styled from 'styled-components';
-import colourValues from '../settings/colours';
-import { Header, Footer } from '@allus-interactive/component-library';
 
-const StyledPage = styled.div`
-  .page {
-  }
+const StyledPage = styled.div``;
+
+const Wrapper = styled.div`
+  width: 100%;
 `;
 
-export function Index() {
-  const linkData = [
-    { url: '/rpgs', text: 'RPGs' },
-    { url: '/platformers', text: 'Platformers' },
-    { url: '/demos-and-prototypes', text: 'Demos/Prototype' },
-  ];
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 768px;
+  padding-bottom: 3rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  color: rgba(55, 65, 81, 1);
+  width: 100%;
+`
 
+export function Index() {
   return (
     <StyledPage>
-      <div className="wrapper">
-        <Header
-          bgColour={colourValues.background}
-          titleColour={colourValues.title}
-          hoverColour={colourValues.white}
-          links={linkData}
-          mobileImageUrl="/images/allus-logo.png"
-          desktopImageUrl="/images/allus-banner.png"
-        />
-        <div className="container">
+      <Wrapper>
+        <Container>
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
               Welcome allus-interactive 👋
             </h1>
           </div>
-        </div>
-        <Footer
-          bgColour={colourValues.background}
-          textColour={colourValues.white}
-        />
-      </div>
+        </Container>
+      </Wrapper>
     </StyledPage>
   );
 }
