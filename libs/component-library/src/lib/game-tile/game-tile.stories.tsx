5019488f68730c3b1bec845d86c5e5a4e689e2fc
Reece <allus.interactive@gmail.com>
2022-07-16 11:52:24 +0100

    created game tile component

--- libs/component-library/src/lib/game-tile/game-tile.stories.tsx
+++ libs/component-library/src/lib/game-tile/game-tile.stories.tsx
@@ -8,6 +8,12 @@ export default {
 
 export const PlatformerGameTile: ComponentStory<typeof GameTile> = () => {
     return (
-        <GameTile />
+        <GameTile 
+            imageUrl='/images/game-tile-placeholder.png'
+            altText='Game Tile Placeholder'
+            url='https://allusinteractive.itch.io'
+            lineOne='Game Tile Component - Line One. A short sentence talking about the game.'
+            lineTwo='Game Tile Component - Line Two. A short sentence talking about the game.'
+        />
     )
 }
--- libs/component-library/src/lib/game-tile/game-tile.tsx
+++ libs/component-library/src/lib/game-tile/game-tile.tsx
@@ -1,3 +1,45 @@
-export const GameTile = () => {
-    return <p>Game Tile Component</p>
+import styled from 'styled-components';
+import breakpointValues from '../../settings/breakpoints';
+
+interface Props {
+    imageUrl: string;
+    altText: string;
+    url: string;
+    lineOne: string;
+    lineTwo: string;
+}
+
+const Wrapper = styled.div`
+    width: 100%;
+    @media (min-width: ${breakpointValues.sm}) {
+        width: 45%;
+        max-width: 500px;
+    }
+`;
+
+const ImageLink = styled.a``;
+
+const Image = styled.img`
+    width: 100%;
+`;
+
+const TextContainer = styled.div`
+    width: 100%;
+    text-align: justify;
+`;
+
+const Text = styled.p``;
+
+export const GameTile = ({imageUrl, altText, url, lineOne, lineTwo}: Props) => {
+    return (
+        <Wrapper>
+            <ImageLink href={url} target='_blank'>
+                <Image src={imageUrl} alt={altText} />
+            </ImageLink>
+            <TextContainer>
+                <Text>{lineOne}</Text>
+                <Text>{lineTwo}</Text>
+            </TextContainer>
+        </Wrapper>
+    )
 };
