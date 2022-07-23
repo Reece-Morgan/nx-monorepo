a5ca7db0c22ceb78aec884afc095aa3ab497a9de
AllusInteractive <AllusInteractive@users.noreply.github.com>
2022-07-23 15:16:46 +0100

    created ongoing-projects page

---
+++ apps/allus-interactive/pages/games/ongoing-projects.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import breakpointValues from '../../settings/breakpoints';
+import colourValues from '../../settings/colours';
+import { GameTile } from '@allus-interactive/component-library';
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 170px);
+`;
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1200px;
+  padding-bottom: 1rem;
+  color: rgba(55, 65, 81, 1);
+  width: 100%;
+  @media (max-width: ${breakpointValues.lg}) {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const FlexContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 10px 0;
+`;
+
+const Title = styled.h1`
+  margin: 10px 0 0 0;
+  color: ${colourValues.title};
+`;
+
+const RpgGamePage = () => {
+    return (
+        <Wrapper>
+            <Container>
+                <Title>Ongoing Projects</Title>
+                <FlexContainer>
+                </FlexContainer>
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default RpgGamePage;
