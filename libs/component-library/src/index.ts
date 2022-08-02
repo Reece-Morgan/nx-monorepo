04926f88e1c43451a775cae48ab92f61dfaa8519
Reece <allus.interactive@gmail.com>
2022-08-02 12:57:11 +0100

    created basic header component

--- libs/component-library/src/index.ts
+++ libs/component-library/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/footer/footer';
 export * from './lib/game-tile/game-tile';
 export * from './lib/header/header';
 export * from './lib/icon/icon';
+export * from './lib/basic-header/basic-header';
