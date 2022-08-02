ebf6170b7154c82fbd8506a5828c4a0a6503b30f
Reece <allus.interactive@gmail.com>
2022-08-02 12:57:56 +0100

    added header to _app

--- apps/portfolio/pages/_app.tsx
+++ apps/portfolio/pages/_app.tsx
@@ -2,15 +2,21 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
+import { BasicHeader } from '@allus-interactive/component-library';
+import colourValues from '../settings/colours';
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to portfolio!</title>
+        <title>Reece Morgan | Portfolio</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <BasicHeader
+        bgColour={colourValues.background}
+        titleColour={colourValues.title}
+        title="Reece Morgan | Portfolio"
+      />
+      <Component {...pageProps} />
     </>
   );
 }
