0586d4064b8144ccf542b3adeeea32ae9488ce1e
Reece <allus.interactive@gmail.com>
2022-08-05 00:00:29 +0100

    deleted basic-header component

--- apps/portfolio/pages/_app.tsx
+++ apps/portfolio/pages/_app.tsx
@@ -2,20 +2,12 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
-import { BasicHeader } from '@allus-interactive/component-library';
-import colourValues from '../settings/colours';
-
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Reece Morgan | Portfolio</title>
       </Head>
-      <BasicHeader
-        bgColour={colourValues.background}
-        titleColour={colourValues.title}
-        title="Reece Morgan | Portfolio"
-      />
       <Component {...pageProps} />
     </>
   );
