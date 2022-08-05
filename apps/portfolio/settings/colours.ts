1158971ea9f55d8befa2442a86e83e59481305f7
Reece <allus.interactive@gmail.com>
2022-08-05 11:58:26 +0100

    temp hover colour

--- apps/portfolio/settings/colours.ts
+++ apps/portfolio/settings/colours.ts
@@ -9,7 +9,7 @@ const colourValues = {
   title: "#dd0207", //#c31014
   primary: "#1F3CA6",
   secondary: "#ffffff",
-  hover: "#1f1f1f", //temp colour
+  hover: "#c31014", //temporary colour - To Be Changes
   black: "#000000",
   white: "#ffffff"
 };
