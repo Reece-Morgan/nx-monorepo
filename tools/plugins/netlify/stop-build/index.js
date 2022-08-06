5bf2d8791a4f34dd28d10a15b8224a2e5485c657
Reece <allus.interactive@gmail.com>
2022-08-06 15:58:13 +0100

    fixed JSON.parse error

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -21,10 +21,10 @@ function projectChanged(currentProject, fromHash, toHash) {
     const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
     const output = execSync(getAffected).toString();
     // get the list of changed projects from the output
-    const changedProjects = JSON.parse(output.projects);
+    const changedProjects = JSON.parse(output).projects;
     if (changedProjects.find(project => project === currentProject)) {
         return true;
     } else {
         return false;
     }
-}
+  }
