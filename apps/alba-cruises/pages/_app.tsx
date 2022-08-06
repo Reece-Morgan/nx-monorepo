fdaf170b26889e5095639aa8929eba86c3b9cd43
Reece <allus.interactive@gmail.com>
2022-08-06 22:46:01 +0100

    updated title

--- apps/alba-cruises/pages/_app.tsx
+++ apps/alba-cruises/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to alba-cruises!</title>
+        <title>Portfolio | Alba Cruises</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
