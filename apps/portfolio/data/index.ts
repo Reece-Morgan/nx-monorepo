a5c8a6022ee6be043d83d0d2c36ebe6580dbf5e9
Reece <allus.interactive@gmail.com>
2022-08-07 15:07:42 +0100

    updated timeline data

--- apps/portfolio/data/index.ts
+++ apps/portfolio/data/index.ts
@@ -15,83 +15,65 @@ export const socialMediaData = [
 export const timeLineData = [
     {
         title: 'First Discovered Web Development',
+        subtitle: 'Hobbyist',
+        description: 'Some description text goes here.',
         date: '2012',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Hobbyist',
-            colour: '#008000'
-        }
+        colour: '#008000',
     },
     {
         title: 'Web Development Hobbyist',
+        subtitle: 'Hobbyist',
+        description: 'Some description text goes here.',
         date: '2012-2014',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Hobbyist',
-            colour: '#008000'
-        }
+        colour: '#008000',
     },
     {
         title: 'HNC Computing',
+        subtitle: 'Student',
+        description: 'Some description text goes here.',
         date: 'Sept 2014 - June 2015',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Student',
-            colour: '#cc0000',
-        }
+        colour: '#e91e63',
     },
     {
         title: 'HND Computing',
+        subtitle: 'Student',
+        description: 'Some description text goes here.',
         date: 'Sept 2015 - June 2016',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Student',
-            colour: '#cc0000',
-        }
+        colour: '#e91e63',
     },
     {
         title: 'HNC Interactive Media',
+        subtitle: 'Student',
+        description: 'Some description text goes here.',
         date: 'Sept 2016 - June 2017',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Student',
-            colour: '#cc0000',
-        }
+        colour: '#e91e63',
     },
     {
         title: 'HND Interactive Media',
+        subtitle: 'Student',
+        description: 'Some description text goes here.',
         date: 'Sept 2017 - June 2018',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Student',
-            colour: '#cc0000',
-        }
+        colour: '#e91e63',
     },
     {
         title: 'The Football Pools - Graduate Developer',
+        subtitle: 'Employment',
+        description: 'Some description text goes here.',
         date: 'June 2018 - Sept 2020',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Employment',
-            colour: '#000d68',
-        }
+        colour: '#000d68',
     },
     {
         title: 'BSc Interactive Media',
+        subtitle: 'Student',
+        description: 'Some description text goes here.',
         date: 'Sept 2018 - June 2019',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Student',
-            colour: '#cc0000',
-        }
+        colour: '#e91e63',
     },
     {
         title: 'The Football Pools - Software Engineer',
+        subtitle: 'Employment',
+        description: 'Some description text goes here.',
         date: 'Sept 2020 - Present',
-        text: 'Some description text goes here.',
-        category: {
-            tag: 'Employment',
-            colour: '#000d68',
-        }
+        colour: '#000d68',
     },
 ]
