b77dc104806fd8a03a00ddfc576ac3d5903d0535
Reece <allus.interactive@gmail.com>
2022-08-07 10:24:43 +0100

    created basic timeline data

--- apps/portfolio/data/index.ts
+++ apps/portfolio/data/index.ts
@@ -11,3 +11,87 @@ export const socialMediaData = [
     { imageUrl: "/images/icons/linkedin.svg", altText: "LinkedIn", url: "https://www.linkedin.com/in/reece-morgan-dev/" },
     { imageUrl: "/images/icons/github.svg", altText: "Github",  url: "https://github.com/AllusInteractive"},
 ];
+
+export const timeLineData = [
+    {
+        title: 'First Discovered Web Development',
+        date: '2012',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Hobbyist',
+            colour: '#008000'
+        }
+    },
+    {
+        title: 'Web Development Hobbyist',
+        date: '2012-2014',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Hobbyist',
+            colour: '#008000'
+        }
+    },
+    {
+        title: 'HNC Computing',
+        date: 'Setp 2014 - June 2015',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Student',
+            colour: '#cc0000',
+        }
+    },
+    {
+        title: 'HND Computing',
+        date: 'Setp 2015 - June 2016',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Student',
+            colour: '#cc0000',
+        }
+    },
+    {
+        title: 'HNC Interactive Media',
+        date: 'Sept 2016 - June 2017',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Student',
+            colour: '#cc0000',
+        }
+    },
+    {
+        title: 'HND Interactive Media',
+        date: 'Sept 2017 - June 2018',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Student',
+            colour: '#cc0000',
+        }
+    },
+    {
+        title: 'The Football Pools - Graduate Developer',
+        date: 'June 2018 - Sept 2020',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Employment',
+            colour: '#000d68',
+        }
+    },
+    {
+        title: 'BSc Interactive Media',
+        date: 'Sept 2018 - June 2019',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Student',
+            colour: '#cc0000',
+        }
+    },
+    {
+        title: 'The Football Pools - Software Engineer',
+        date: 'Sept 2020 - Present',
+        text: 'Some description text goes here.',
+        category: {
+            tag: 'Employment',
+            colour: '#000d68',
+        }
+    },
+]
