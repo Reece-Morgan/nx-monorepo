a3d10bd5f7750d2f69217017e5f7358424302011
Reece <allus.interactive@gmail.com>
2022-08-11 17:13:47 +0100

    added hardcoded api call

--- apps/movie-search/pages/index.tsx
+++ apps/movie-search/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import colourValues from '../settings/colours';
 import { MovieList } from '../src/components/movie-list';
@@ -10,8 +10,6 @@ const PageWrapper = styled.div`
 `;
 
 const Container = styled.div`
-  background-color: ${colourValues.black};
-  color: ${colourValues.white};
   min-height: auto;
   height: 100vh;
   min-height: 100vh;
@@ -30,27 +28,23 @@ const Wrapper = styled.div`
 `;
 
 const HomePage = () => {
-  const [movies, setMovies] = useState([{
-    "Title": "Star Wars: Episode IV - A New Hope",
-    "Year": "1977",
-    "imdbID": "tt0076759",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  },
-  {
-    "Title": "Star Wars: Episode V - The Empire Strikes Back",
-    "Year": "1980",
-    "imdbID": "tt0080684",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  },
-  {
-    "Title": "Star Wars: Episode VI - Return of the Jedi",
-    "Year": "1983",
-    "imdbID": "tt0086190",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  }]);
+  const [movies, setMovies] = useState([]);
+
+  const getMovieRequest = async () => {
+    const apiKey = '5fd15d12';
+    const url = `http://www.omdbapi.com/?s=star wars&apiKey=${apiKey}`;
+
+    const data = await fetch(url);
+    const response = await data.json();
+
+    if (response.Search) {
+      setMovies(response.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest();
+  }, []);
 
   return (
     <PageWrapper>
