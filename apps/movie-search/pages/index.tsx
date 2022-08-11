48a7dcc4afc11c804895e69795c49a9bffbd1009
Reece <allus.interactive@gmail.com>
2022-08-11 17:02:44 +0100

    added movie-list to index

--- apps/movie-search/pages/index.tsx
+++ apps/movie-search/pages/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import colourValues from '../settings/colours';
+import { MovieList } from '../src/components/movie-list';
 
 const PageWrapper = styled.div`
   width: 100%;
@@ -18,8 +20,8 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-evenly;
   text-align: center;
   max-width: 1270px;
   margin: 0 auto;
@@ -28,11 +30,33 @@ const Wrapper = styled.div`
 `;
 
 const HomePage = () => {
+  const [movies, setMovies] = useState([{
+    "Title": "Star Wars: Episode IV - A New Hope",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+  },
+  {
+    "Title": "Star Wars: Episode V - The Empire Strikes Back",
+    "Year": "1980",
+    "imdbID": "tt0080684",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+  },
+  {
+    "Title": "Star Wars: Episode VI - Return of the Jedi",
+    "Year": "1983",
+    "imdbID": "tt0086190",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+  }]);
+
   return (
     <PageWrapper>
       <Container>
         <Wrapper>
-          <h1>The Movie Search Engine!</h1>
+          <MovieList movies={movies} />
         </Wrapper>
       </Container>
     </PageWrapper>
