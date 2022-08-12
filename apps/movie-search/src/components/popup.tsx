048099861ab3e6981aba12dd2bc1dc49f8a215a3
Reece <allus.interactive@gmail.com>
2022-08-12 16:43:43 +0100

    Set up popup to dynamically show content

--- apps/movie-search/src/components/popup.tsx
+++ apps/movie-search/src/components/popup.tsx
@@ -1,12 +1,48 @@
+import breakpointValues from "apps/movie-search/settings/breakpoints";
 import colourValues from "apps/movie-search/settings/colours";
 import styled from "styled-components"
 
 interface Props {
     display: string;
+    title: string;
+    content: React.ReactNode;
     onClick: () => void;
 }
 
-const Wrapper = styled.div``;
+// TODO: remove max-width and min-height
+const Wrapper = styled.div<{ display: string }>`
+    max-width: 400px; 
+    min-height: 600px;
+    background-color: ${colourValues.white};
+    position: relative;
+    margin: 0 auto;
+    border-radius: 15px;
+    z-index: 21;
+    display: ${(props) => props.display ? props.display : 'none'};
+`;
+
+const Header = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const Title = styled.h1`
+    font-size: 1em;
+    @media (min-width: ${breakpointValues.xs}) {
+        font-size: 1.5em;
+    }
+`;
+
+const Span = styled.span`
+    font-size: 1em;
+    font-weight: 600;
+    padding: 0 5px;
+    cursor: pointer;
+    @media (min-width: ${breakpointValues.xs}) {
+        font-size: 1.5em;
+    }
+`;
 
 const Overlay = styled.div<{ display: string }>`
     background-color: ${colourValues.black};
@@ -15,15 +51,23 @@ const Overlay = styled.div<{ display: string }>`
     right: 0;
     bottom: 0;
     left: 0;
-    z-index: 20;
     opacity: 0.5;
-    display: ${(props) => props.display ? props.display : 'none'}
+    z-index: 20;
+    display: ${(props) => props.display ? props.display : 'none'};
 `;
 
-export const Popup = ({ display, onClick }: Props) => {
+export const Popup = ({ display, title, content, onClick }: Props) => {
     return (
-        <Wrapper>
+        <>
+            <Wrapper display={display}>
+                <Header>
+                    <span></span>
+                    <Title>{title}</Title>
+                    <Span onClick={onClick}>X</Span>
+                </Header>
+                {content}
+            </Wrapper>
             <Overlay display={display} onClick={onClick}/>
-        </Wrapper>
+        </>
     )
 }
