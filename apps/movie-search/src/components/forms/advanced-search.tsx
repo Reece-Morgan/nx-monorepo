f72b4e765d3795b78d0a8e6eb0e0f632336dd6ae
Reece <allus.interactive@gmail.com>
2022-08-12 20:45:41 +0100

    Built out form

--- apps/movie-search/src/components/forms/advanced-search.tsx
+++ apps/movie-search/src/components/forms/advanced-search.tsx
@@ -1,6 +1,14 @@
 import breakpointValues from "apps/movie-search/settings/breakpoints";
 import colourValues from "apps/movie-search/settings/colours";
 import styled from "styled-components"
+import { useForm } from 'react-hook-form';
+
+interface Props {
+    title: string;
+    year: number;
+    type: string;
+    rating: string;
+}
 
 const Wrapper = styled.div`
     padding: 20px 10%;
@@ -25,34 +33,127 @@ const Input = styled.input`
     }
 `;
 
+const ErrorMsg = styled.p`
+    font-size: 1em;
+    color: ${colourValues.error};
+    margin: 0;
+    text-align: left;
+    padding: 5px 0 0 0;
+`;
+
+const ButtonWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: ${breakpointValues.xs}) {
+        flex-direction: row;
+        justify-content: space-around;
+    }
+`;
+
 const Button = styled.button`
     background-color: ${colourValues.black};
     color: ${colourValues.white};
     border: 2px solid ${colourValues.black};
     border-radius: 10px;
-    width: 50%;
-    margin: 0 auto;
+    width: 75%;
+    margin: 5px auto;
     cursor: pointer;
+    padding: 5px;
 
     :hover {
         background-color: ${colourValues.white};
         color: ${colourValues.black};
     }
+
+    @media (min-width: ${breakpointValues.xs}) {
+        width: 40%;
+    }
+`;
+
+const AltButton = styled(Button)`
+    background-color: ${colourValues.white};
+    color: ${colourValues.black};
+    border: 2px solid ${colourValues.black};
+
+    :hover {
+        background-color: ${colourValues.black};
+        color: ${colourValues.white};
+    }
+`;
+
+const Span = styled.span`
+    font-size: 0.75em;
 `;
 
 export const AdvancedSearch =() => {
+    const { register, handleSubmit, errors } = useForm<Props>();
+
+    const onSubmit = handleSubmit(data => {
+        console.log('Form Data: ' + JSON.stringify(data));
+    });
+
     return (
         <Wrapper>
-            <Form>
-                <Label htmlFor='title'>Title:</Label>
-                <Input type='text' id='title' name='title' placeholder='Star Wars' required />
-                <Label htmlFor='year'>Year:</Label>
-                <Input type='numeric' id='year' name='year' placeholder='1977' required />
-                <Label htmlFor='type'>Type:</Label>
-                <Input type='text' id='type' name='type' placeholder='Movie' required />
+            <Form onSubmit={onSubmit}>
+                <Label htmlFor='title'>Title:*</Label>
+                <Input 
+                    type='text' 
+                    id='title' 
+                    name='title' 
+                    placeholder='Star Wars'
+                    ref={register({
+                        required: {
+                            value: true,
+                            message: 'This is required',
+                        },
+                    })}
+                />
+                <ErrorMsg>{errors.title && 'Please enter a title'}</ErrorMsg>
+                <Label htmlFor='year'>Year:*</Label>
+                <Input 
+                    type='numeric' 
+                    maxLength={4} 
+                    id='year' 
+                    name='year' 
+                    placeholder='1977'
+                    ref={register({
+                        required: {
+                            value: true,
+                            message: 'This is required',
+                        },
+                        pattern: { value: /[0-9]{4}/, message: ''},
+                    })}
+                />
+                <ErrorMsg>{errors.year && 'Please enter a valid year'}</ErrorMsg>
+                <Label htmlFor='type'>Media Type:*</Label>   {/* TODO - change input to dropdown/enum */}
+                <Input 
+                    type='text' 
+                    id='type' 
+                    name='type' 
+                    placeholder='Movie'
+                    ref={register({
+                        required: {
+                            value: true,
+                            message: 'This is required',
+                        },
+                    })}
+                />
+                <ErrorMsg>{errors.type && 'Please choose a media type'}</ErrorMsg>
                 <Label htmlFor='rating'>Rating:</Label>
-                <Input type='text' id='rating' name='rating' placeholder='PG' required />
-                <Button type="submit" value="Submit">Search</Button>
+                <Input 
+                    type='text' 
+                    id='rating' 
+                    name='rating' 
+                    placeholder='PG'
+                    ref={register()}
+                />
+                <ErrorMsg>{errors.title && 'Please enter a rating'}</ErrorMsg>
+                <Span>* Required</Span>
+                <ButtonWrapper>
+                    <Button>Search</Button>
+                    <AltButton>Clear</AltButton>
+                </ButtonWrapper>
             </Form>
         </Wrapper>    
     )
