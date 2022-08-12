2c5db8a96bdf65bfbfdb68a6302cbc7ab7a408ac
Reece <allus.interactive@gmail.com>
2022-08-12 09:31:44 +0100

    styling added to header and search box

--- apps/movie-search/src/components/movie-list-heading.tsx
+++ apps/movie-search/src/components/movie-list-heading.tsx
@@ -1,7 +1,23 @@
+import breakpointValues from "apps/movie-search/settings/breakpoints";
+import styled from "styled-components"
+
+const Wrapper = styled.header`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Title = styled.h1`
+    font-size: 1em;
+    @media (min-width: ${breakpointValues.xs}) {
+        font-size: 1.5em;
+    }
+`;
+
 export const MovieListHeading = () => {
     return (
-        <div>
-            <h1>Movie Search Engine</h1>
-        </div>
+        <Wrapper>
+            <Title>Movie Search Engine</Title>
+        </Wrapper>
     )
 }
--- apps/movie-search/src/components/search-box.tsx
+++ apps/movie-search/src/components/search-box.tsx
@@ -1,18 +1,38 @@
-import { Dispatch, SetStateAction } from "react";
+import breakpointValues from "apps/movie-search/settings/breakpoints";
+import colourValues from "apps/movie-search/settings/colours";
+import styled from "styled-components";
 
 interface Props {
     value: string;
     onChange: (value: string) => void;
 }
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Input = styled.input`
+    padding: 5px 0;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid ${colourValues.white};
+    border-radius: 5px;
+    box-sizing: border-box;
+    @media (min-width: ${breakpointValues.xs}) {
+        padding: 5px 20px;
+    }
+`;
+
 export const SearchBox = ({ value, onChange }: Props) => {
     return (
-        <div>
-            <input 
+        <Wrapper>
+            <Input 
                 defaultValue={value}
                 onChange={(event) => onChange(event.target.value)}
                 placeholder='Start typing to search...'
             />
-        </div>
+        </Wrapper>
     )
 }
