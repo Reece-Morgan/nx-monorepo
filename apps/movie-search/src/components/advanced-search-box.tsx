80015ef1e3541b1893944eee914c71917da09edb
Reece <allus.interactive@gmail.com>
2022-08-17 10:26:58 +0100

    added basic search to page

--- apps/movie-search/src/components/advanced-search-box.tsx
+++ apps/movie-search/src/components/advanced-search-box.tsx
@@ -1,6 +1,7 @@
 import breakpointValues from "apps/movie-search/settings/breakpoints";
 import colourValues from "apps/movie-search/settings/colours";
 import styled from "styled-components";
+import { BasicSearch } from "./forms/basic-search";
 
 interface Props {
     value: string;
@@ -27,27 +28,24 @@ const Input = styled.input`
 `;
 
 const Button = styled.button`
-    background-color: ${colourValues.white};
-    color: ${colourValues.black};
+    background-color: ${colourValues.black};
+    color: ${colourValues.white};
     border: 2px solid ${colourValues.white};
     border-radius: 10px;
     width: 100%;
     cursor: pointer;
-
+    max-height: 30px;
+    height: 30px;
     :hover {
-        background-color: ${colourValues.black};
-        color: ${colourValues.white};
+        background-color: ${colourValues.white};
+        color: ${colourValues.black};
     }
 `;
 
 export const AdvancedSearchBox = ({ value, onChange, onClick }: Props) => {
     return (
         <Wrapper>
-            <Input 
-                defaultValue={value}
-                onChange={(event) => onChange(event.target.value)}
-                placeholder='Start typing to search...'
-            />
+            <BasicSearch />
             <Button onClick={onClick}>Advanced Search</Button>
         </Wrapper>
     )
