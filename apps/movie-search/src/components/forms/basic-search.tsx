aea559ad4f775557278ae6b251479d7d83881ec4
Reece <allus.interactive@gmail.com>
2022-08-17 10:26:39 +0100

    created form for basic search

---
+++ apps/movie-search/src/components/forms/basic-search.tsx
@@ -0,0 +1,74 @@
+import breakpointValues from "apps/movie-search/settings/breakpoints";
+import colourValues from "apps/movie-search/settings/colours";
+import styled from "styled-components"
+import { useForm } from 'react-hook-form';
+
+interface Props {
+    title: string;
+}
+
+const Wrapper = styled.div``;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: row;
+`;
+
+const Input = styled.input`
+    padding: 5px 0;
+    margin: 8px 5px 5px 0;
+    display: inline-block;
+    max-height: 30px;
+    height: 30px;
+    border: 1px solid ${colourValues.black};
+    border-radius: 5px;
+    box-sizing: border-box;
+    width: 50%;
+    @media (min-width: ${breakpointValues.xs}) {
+        padding: 5px 20px;
+    }
+`;
+
+const Button = styled.button`
+    background-color: ${colourValues.white};
+    color: ${colourValues.black};
+    border: 2px solid ${colourValues.white};
+    border-radius: 10px;
+    width: 45%;
+    margin: 8px 0 5px 5px;
+    cursor: pointer;
+    padding: 5px;
+    max-height: 30px;
+    height: 30px;
+    :hover {
+        background-color: ${colourValues.black};
+        color: ${colourValues.white};
+    }
+
+    @media (min-width: ${breakpointValues.xs}) {
+        width: 50%;
+    }
+`;
+
+export const BasicSearch =() => {
+    const { register, handleSubmit } = useForm<Props>();
+
+    const onSubmit = handleSubmit(data => {
+        console.log('Form Data: ' + JSON.stringify(data));
+    });
+
+    return (
+        <Wrapper>
+            <Form onSubmit={onSubmit}>
+                <Input 
+                    type='text' 
+                    id='title' 
+                    name='title' 
+                    placeholder='Search...'
+                    ref={register()}
+                />
+                <Button>Search</Button>
+            </Form>
+        </Wrapper>    
+    )
+}
