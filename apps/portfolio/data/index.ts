0b2253da01348995184430682b5c0eddf80facfb
Reece <reecemorgandev@gmail.com>
2023-07-03 21:41:29 +0100

    added sketchfab to footer links

--- apps/portfolio/data/index.ts
+++ apps/portfolio/data/index.ts
@@ -9,7 +9,8 @@ export const socialMediaData = [
     { imageUrl: "/images/icons/twitter.svg", altText: "Twitter", url: "https://twitter.com/reecemorgandev" },
     { imageUrl: "/images/icons/instagram.svg", altText: "Instagram", url: "https://www.instagram.com/reecemorgandev/" },
     { imageUrl: "/images/icons/linkedin.svg", altText: "LinkedIn", url: "https://www.linkedin.com/in/reece-morgan-dev/" },
-    { imageUrl: "/images/icons/github.svg", altText: "Github",  url: "https://github.com/AllusInteractive"},
+    { imageUrl: "/images/icons/github.svg", altText: "Github", url: "https://github.com/Reece-Morgan" },
+    { imageUrl: "/images/icons/sketchfab.svg", altText: "Sketchfab", url: "https://sketchfab.com/reecemorgan" },
 ];
 
 export const timeLineData = [
