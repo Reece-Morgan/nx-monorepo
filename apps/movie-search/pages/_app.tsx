9dbd3dbddc637d685c27aec7a9d17f7d21025a84
Reece Morgan <reece.morgan@footballpools.com>
2024-04-12 12:19:12 +0100

    set up redux store

--- apps/movie-search/pages/_app.tsx
+++ apps/movie-search/pages/_app.tsx
@@ -1,18 +1,23 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from '../src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Reece Morgan | Portfolio | Movie Search</title>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
+    <Provider store={store}>
+      <PersistGate loading={undefined} persistor={persistor}>
+        <Head>
+          <title>Reece Morgan | Portfolio | Movie Search</title>
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </PersistGate>
+    </Provider>
   );
 }
 
---
+++ apps/movie-search/src/types/types.ts
@@ -0,0 +1,7 @@
+export type Movie = {
+  imdbID: string;
+  Title: string;
+  Year: string;
+  Type: string;
+  Poster: string;
+};
