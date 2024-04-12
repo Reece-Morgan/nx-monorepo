00ac680801f90aefde6ba1962c59871bea47d25f
Reece Morgan <reece.morgan@footballpools.com>
2024-04-12 12:19:49 +0100

    updated favourites to add/remove from redux

--- apps/movie-search/pages/index.tsx
+++ apps/movie-search/pages/index.tsx
@@ -1,14 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import breakpointValues from '../settings/breakpoints';
 import colourValues from '../settings/colours';
 import { AddFavourite } from '../src/components/add-favourite';
-import { AdvancedSearch } from '../src/components/forms/advanced-search';
 import { MovieList } from '../src/components/movie-list';
 import { MovieListHeading } from '../src/components/movie-list-heading';
 import { Popup } from '../src/components/popup';
 import { RemoveFavourite } from '../src/components/remove-favourite';
 import { AdvancedSearchBox } from '../src/components/advanced-search-box';
+import { useDispatch } from 'react-redux';
+import {
+  addMovieToFavourites,
+  removeMovieFromFavourites,
+} from '../src/redux/favourites/favourites';
+import { useAppSelector } from '../src/redux/store';
 
 const PageWrapper = styled.div`
   width: 100%;
@@ -72,66 +77,27 @@ const Text = styled.p`
   }
 `;
 
-const SmallText = styled.p`
-  font-size: 0.75em;
-  padding: 10px;
-  @media (min-width: ${breakpointValues.xs}) {
-    font-size: 1em;
-  }
-`;
-
 const SearchAdvanced = () => {
+  const dispatch = useDispatch();
   const [movies, setMovies] = useState([]);
-  const [favourites, setFavourites] = useState([]);
   const [popupDisplay, setPopupDisplay] = useState<'none' | 'block'>('none');
+  const { movies: favourites } = useAppSelector((state) => state.Favourites);
 
   const togglePopup = () => {
     setPopupDisplay(popupDisplay === 'none' ? 'block' : 'none');
-  }
+  };
 
   const setSearchValue = (value) => {
     setMovies(value);
-  }
+  };
 
   const addFavouriteMovie = (movie) => {
-    const favouriteList = [...favourites, movie];
-
-    const checkFavourites = favourites.filter(
-      (favourite) => favourite.imdbID === movie.imdbID
-    );
-
-    if (checkFavourites.length === 1) {
-      return;
-    }
-
-    setFavourites(favouriteList);
-    saveToLocalStorage(favouriteList);
-  }
+    dispatch(addMovieToFavourites(movie));
+  };
 
   const removeFavouriteMovie = (movie) => {
-    const favouriteList = favourites.filter(
-      (favourite) => favourite.imdbID !== movie.imdbID
-    );
-
-    setFavourites(favouriteList);
-    saveToLocalStorage(favouriteList);
-  }
-
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem('rm-portfolio-movie-favourites', JSON.stringify(items));
-  }
-
-  useEffect(() => {
-    const userFavourites = JSON.parse(
-      localStorage.getItem('rm-portfolio-movie-favourites')
-    );
-
-    if (userFavourites === null) {
-      return;
-    }
-
-    setFavourites(userFavourites);
-  }, []);
+    dispatch(removeMovieFromFavourites(movie));
+  };
 
   return (
     <PageWrapper>
@@ -139,33 +105,53 @@ const SearchAdvanced = () => {
         <Header>
           <HeaderWrapper>
             <MovieListHeading />
-            <AdvancedSearchBox setMovies={setSearchValue} togglePopup={togglePopup} />
+            <AdvancedSearchBox
+              setMovies={setSearchValue}
+              togglePopup={togglePopup}
+            />
           </HeaderWrapper>
         </Header>
         <Wrapper>
           {movies.length === 0 && favourites.length === 0 && (
             <>
-              <Text>Use the search bar to look up any movie, TV show or video game</Text>
-              <Text>Click Add to Favourites to keep track of all your favourites!</Text>
+              <Text>
+                Use the search bar to look up any movie, TV show or video game
+              </Text>
+              <Text>
+                Click Add to Favourites to keep track of all your favourites!
+              </Text>
             </>
           )}
           {favourites && favourites.length > 0 && (
             <MovieWrapper>
               <Title>Favourites</Title>
-              <MovieList movies={favourites} favourites={<RemoveFavourite />} onClick={removeFavouriteMovie}/>
+              <MovieList
+                movies={favourites}
+                favourites={<RemoveFavourite />}
+                onClick={removeFavouriteMovie}
+              />
             </MovieWrapper>
           )}
           {movies && movies.length > 0 && (
             <MovieWrapper>
               <Title>Search Results</Title>
-              <MovieList movies={movies} favourites={<AddFavourite />} onClick={addFavouriteMovie}/>
+              <MovieList
+                movies={movies}
+                favourites={<AddFavourite />}
+                onClick={addFavouriteMovie}
+              />
             </MovieWrapper>
           )}
         </Wrapper>
-        <Popup display={popupDisplay} title='Advanced Search' closePopup={togglePopup} setMovies={setSearchValue}/>
+        <Popup
+          display={popupDisplay}
+          title="Advanced Search"
+          closePopup={togglePopup}
+          setMovies={setSearchValue}
+        />
       </Container>
     </PageWrapper>
-  )
-}
+  );
+};
 
 export default SearchAdvanced;
